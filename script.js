bc3901b432015f31f478c1966ba360c60b5ddd8b
Aaron Krajeski <ak@aaronk.is>
2017-07-15 14:53:13 -0400

    Small refactor

--- script.js
+++ script.js
@@ -10,12 +10,14 @@ var camera;
 var scene;
 
 var playCounter = 0;
-var audioClips;
+var audioClips = document.getElementsByClassName("audioClip");
+var videoClips = document.getElementsByClassName("videoClip");
+var videoTextures = [];
 var playHead = 0;
 var controls;
 var size;
 var mousePosition = new THREE.Vector2();
-var materials = [];
+var barMaterials = [];
 
 function makeBarMaterial(options) {
   return new THREE.ShaderMaterial({
@@ -64,31 +66,17 @@ function init() {
   renderer.setPixelRatio( window.devicePixelRatio );
   renderer.setSize( window.innerWidth, window.innerHeight );
   container.appendChild( renderer.domElement );
-  video = document.getElementById('base-video');
-  video2 = document.getElementById('second-video');
 
-  videoTexture = new THREE.VideoTexture(video);
-  videoTexture.minFilter = THREE.NearestFilter;
-  videoTexture.magFilter = THREE.NearestFilter;
-  videoTexture.format = THREE.RGBFormat;
-
-  videoTexture2 = new THREE.VideoTexture(video2);
-  videoTexture2.minFilter = THREE.NearestFilter;
-  videoTexture2.magFilter = THREE.NearestFilter;
-  videoTexture2.format = THREE.RGBFormat;
+  for (var i = 0; i < videoClips.length; i++) {
+    var vt = new THREE.VideoTexture(videoClips[i]);
+    vt.minFilter = vt.magFilter = THREE.NearestFilter;
+    vt.format = THREE.RGBFormat;
+    videoTextures.push(vt);
+  }
 
   camera = new THREE.OrthographicCamera(-0.5, 0.5, 0.5, -0.5, 0.1, 100000);
   camera.position.z = 50;
   scene = new THREE.Scene();
-
-  audioClips = document.getElementsByClassName("audioClip");
-
-  // var planeGeometry = new THREE.PlaneGeometry(2, 2);
-  // // var mat = new THREE.MeshBasicMaterial({map:videoTexture});
-  // // var mat = new THREE.MeshBasicMaterial({color: new THREE.Color("pink")});
-  // material = makeBarMaterial();
-  // var mesh = new THREE.Mesh(planeGeometry, material);
-  // scene.add(mesh);
 
   size = renderer.getSize();
   var dpr = renderer.getPixelRatio();
@@ -97,26 +85,27 @@ function init() {
     var hue = Math.random() * 255;
     var mat = makeBarMaterial({
       color: new THREE.Color("hsl("+hue+", 70%, 50%)"),
-      video: videoTexture,
+      video: videoTextures[0],
       resolution: new THREE.Vector2(size.width * dpr, size.height * dpr),
     });
     var mesh = new THREE.Mesh(geo, mat);
     mesh.position.x = THREE.Math.mapLinear(i, 0, audioClips.length, -0.5, 0.5);
     mesh.position.x += 0.5/audioClips.length;
     scene.add(mesh);
-    materials.push(mat);
+    barMaterials.push(mat);
   }
 }
 
 function addPlayCounter() {
   playCounter++;
   console.log(playCounter);
-  if (playCounter == audioClips.length + 2) {
+  if (playCounter == audioClips.length + videoClips.length) {
     for (var i = 0; i < audioClips.length; i++) {
       audioClips[i].play();
     }
-    video.play();
-    video2.play();
+    for (var i = 0; i < videoClips.length; i++) {
+      videoClips[i].play();
+    }
   }
 }
 
@@ -146,7 +135,6 @@ function animate() {
 
 document.addEventListener("click", onDocumentClick, false );
 document.addEventListener("mousemove", onDocumentMouseMove, false );
-// var projector = new THREE.Projector();
 function onDocumentClick(event) {
   event.preventDefault();
 
@@ -161,11 +149,11 @@ function onDocumentClick(event) {
     audioClips[hoverOver].volume = 1;
   }
 
-  materials[hoverOver].uniforms.u_playing.value = !materials[hoverOver].uniforms.u_playing.value;
-  if (materials[hoverOver].uniforms.u_playing.value === true) {
-    materials[hoverOver].uniforms.u_videoTexture.value = videoTexture;
+  barMaterials[hoverOver].uniforms.u_playing.value = !barMaterials[hoverOver].uniforms.u_playing.value;
+  if (barMaterials[hoverOver].uniforms.u_playing.value === true) {
+    barMaterials[hoverOver].uniforms.u_videoTexture.value = videoTextures[0];
   } else {
-    materials[hoverOver].uniforms.u_videoTexture.value = videoTexture2;
+    barMaterials[hoverOver].uniforms.u_videoTexture.value = videoTextures[1];
   }
 }
 
@@ -178,11 +166,11 @@ function onDocumentMouseMove(event) {
 
   var hoverOver = Math.floor(mousePosition.x * audioClips.length);
 
-  for (var i = 0; i < materials.length; i++) {
-    materials[i].uniforms.u_mousePos.value = mousePosition;
-    materials[i].uniforms.u_mouseOver.value = false;
+  for (var i = 0; i < barMaterials.length; i++) {
+    barMaterials[i].uniforms.u_mousePos.value = mousePosition;
+    barMaterials[i].uniforms.u_mouseOver.value = false;
     if (i == hoverOver) {
-      materials[i].uniforms.u_mouseOver.value = true;
+      barMaterials[i].uniforms.u_mouseOver.value = true;
     }
   }
 }
