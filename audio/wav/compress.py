e44b6049f360a35eb1fb084951af9567cc16869b
Aaron Krajeski <ak@aaronk.is>
2017-10-01 11:58:35 -0400

    add compression script

---
+++ audio/wav/compress.py
@@ -0,0 +1,12 @@
+import os
+
+maxBR = 32
+os.listdir('.')
+files = [x for x in os.listdir('.') if x.endswith('wav')]
+
+cmd = "lame -V 9 -b 0 -B {maxBR} {name}.wav {name}.mp3"
+
+for f in files:
+  basename = f.split('.')[0]
+  fullCmd = cmd.format(maxBR=maxBR, name=basename)
+  os.system(fullCmd)
